1ad442d7510b1431977d9d5d49a5f85bb61d74fb
Christian Brindley <christian.brindley@forgerock.com>
2023-06-27 15:10:10 +0100

    Handle push of audit when config not present

--- packages/fr-config-push/src/scripts/update-audit.js
+++ packages/fr-config-push/src/scripts/update-audit.js
@@ -1,11 +1,18 @@
 const { readFile } = require("fs/promises");
 const path = require("path");
 const fidcRequest = require("../helpers/fidc-request");
+const fs = require("fs");
 
 const updateAudit = async (argv, token) => {
   const { TENANT_BASE_URL, CONFIG_DIR } = process.env;
   try {
     const dir = path.join(CONFIG_DIR, "/audit");
+
+    if (!fs.existsSync(dir)) {
+      console.log("Warning: no audit config defined");
+      return;
+    }
+
     const fileContent = JSON.parse(
       await readFile(path.join(dir, "audit.json"))
     );

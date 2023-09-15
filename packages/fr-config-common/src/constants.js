9f52ef8a763ca9804de8c4e1112564289fdfb822
Christian Brindley <christian.brindley@forgerock.com>
2023-09-15 14:20:37 +0100

    cleanup FILENAME_SAFE_CHAR

--- packages/fr-config-common/src/constants.js
+++ packages/fr-config-common/src/constants.js
@@ -3,7 +3,4 @@ const PrivateKeyFormat = {
   PEM: "PEM",
 };
 
-const FILENAME_SAFE_CHAR = "_";
-
 module.exports.PrivateKeyFormat = PrivateKeyFormat;
-module.exports.FILENAME_SAFE_CHAR = FILENAME_SAFE_CHAR;

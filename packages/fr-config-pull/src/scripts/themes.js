69734f6ae6eeadf24564a99ba2017d4d38502502
christian-brindley <christian.brindley@forgerock.com>
2023-09-18 11:15:25 +0100

    55: Support for themes with locale settings (#57)
    Resolves: https://github.com/ForgeRock/fr-config-manager/issues/55

--- packages/fr-config-pull/src/scripts/themes.js
+++ packages/fr-config-pull/src/scripts/themes.js
@@ -2,10 +2,12 @@ const utils = require("../helpers/utils.js");
 const fs = require("fs");
 const axios = require("axios");
 const { saveJsonToFile } = utils;
+const path = require("path");
 
 const EXPORT_SUB_DIR = "themes";
-
-const HTML_FIELDS = ["accountFooter", "journeyFooter", "journeyHeader"];
+const {
+  THEME_HTML_FIELDS,
+} = require("../../../fr-config-common/src/constants.js");
 
 function processThemes(themes, fileDir, name) {
   try {
@@ -19,13 +21,44 @@ function processThemes(themes, fileDir, name) {
         fs.mkdirSync(themePath, { recursive: true });
       }
 
-      for (const field of HTML_FIELDS) {
-        const fieldFilename = `${field}.html`;
-        const breakoutFile = `${themePath}/${fieldFilename}`;
-        fs.writeFileSync(breakoutFile, theme[field]);
-        theme[field] = {
-          file: fieldFilename,
-        };
+      for (const field of THEME_HTML_FIELDS) {
+        switch (typeof theme[field]) {
+          case "string":
+            {
+              const fieldFilename = `${field}.html`;
+              const breakoutFile = path.join(themePath, fieldFilename);
+              fs.writeFileSync(breakoutFile, theme[field]);
+              theme[field] = {
+                file: fieldFilename,
+              };
+            }
+            break;
+
+          case "object":
+            const fieldPath = path.join(themePath, field);
+            if (!fs.existsSync(fieldPath)) {
+              fs.mkdirSync(fieldPath, { recursive: true });
+            }
+
+            Object.keys(theme[field]).forEach((locale) => {
+              {
+                const localeFilename = path.join(field, `${locale}.html`);
+                const breakoutFile = path.join(themePath, localeFilename);
+                fs.writeFileSync(breakoutFile, theme[field][locale]);
+                theme[field][locale] = {
+                  file: localeFilename,
+                };
+              }
+            });
+            break;
+
+          default:
+            console.error(
+              "Unexpected locale type in theme",
+              typeof theme[field]
+            );
+            process.exit(1);
+        }
       }
 
       const fileName = `${themePath}/${theme.name}.json`;

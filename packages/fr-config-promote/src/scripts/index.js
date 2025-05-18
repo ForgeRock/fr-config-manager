730470962383b3c5c16e04f080ffa5b1e2c95028
Christian Brindley <christian.brindley@forgerock.com>
2025-05-18 14:27:41 +0100

    257: Ignore encrypted secrets Resolves: https://github.com/ForgeRock/fr-config-manager/issues/257

--- packages/fr-config-promote/src/scripts/index.js
+++ packages/fr-config-promote/src/scripts/index.js
@@ -1,16 +1,14 @@
-const checkTenantsLocked= require("./check-tenants-locked");
-const lockTenants= require("./lock-tenants");
-const unlockTenants= require("./unlock-tenants");
-const checkPromotionStatus= require("./check-promotion-status");
-const runDryRunPromotion= require("./run-promotion-dryrun");
-const runPromotion= require("./run-promotion");
-const checkPromotionReports= require("./check-promotion-reports");
+const checkTenantsLocked = require("./check-tenants-locked");
+const lockTenants = require("./lock-tenants");
+const unlockTenants = require("./unlock-tenants");
+const checkPromotionStatus = require("./check-promotion-status");
+const runPromotion = require("./run-promotion");
+const checkPromotionReports = require("./check-promotion-reports");
 module.exports = {
   checkTenantsLocked,
   lockTenants,
   unlockTenants,
   checkPromotionStatus,
-  runDryRunPromotion,
   runPromotion,
   checkPromotionReports,
 };
--- packages/fr-config-promote/src/scripts/run-promotion.js
+++ packages/fr-config-promote/src/scripts/run-promotion.js
@@ -1,33 +1,30 @@
-const {
-  env
-} = require("yargs");
-const {
-  restPost,
-} = require("../../../fr-config-common/src/restClient");
+const { restPost } = require("../../../fr-config-common/src/restClient");
+const { OPTION } = require("../helpers/cli-options");
 
-const runPromotion = async (argv, token) => {
-  const {
-      TENANT_ENV_UPPER_FQDN
-  } = process.env;
+const runPromotion = async (argv, dryRun, token) => {
+  const { TENANT_ENV_UPPER_FQDN } = process.env;
+  const ignoreEncryptedSecrets = argv[OPTION.IGNORE_ENCRYPTED_SECRETS]
+    ? true
+    : false;
 
   try {
-      const envUrl = `${TENANT_ENV_UPPER_FQDN}/environment`;
-      const dryRunJSON = {
-          "dryRun": false
-      };
-      const response = await restPost(
-          `${envUrl}/promotion/promote`,
-          null,
-          dryRunJSON,
-          token,
-          "protocol=2.1,resource=1.0"
-      );
+    const envUrl = `${TENANT_ENV_UPPER_FQDN}/environment`;
+    const body = {
+      dryRun: dryRun,
+      ignoreEncryptedSecrets: ignoreEncryptedSecrets,
+    };
+    const response = await restPost(
+      `${envUrl}/promotion/promote`,
+      null,
+      body,
+      token,
+      "protocol=2.1,resource=1.0"
+    );
 
-      console.log(JSON.stringify(response.data,null,4));
-
+    console.log(JSON.stringify(response.data, null, 4));
   } catch (error) {
-      console.error(error.message);
-      process.exit(1);
+    console.error(error.message);
+    process.exit(1);
   }
 };
 

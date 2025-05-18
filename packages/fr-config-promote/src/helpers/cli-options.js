14a0e021ab781649f31903aec59d0e6ece5d61d2
christian-brindley <christian.brindley@forgerock.com>
2025-05-18 14:36:14 +0100

    257 ignore encrypted secrets (#264)
    * 257: Ignore encrypted secrets
    Resolves: https://github.com/ForgeRock/fr-config-manager/issues/257
    
    * 257: Ignore encrypted secrets - readme

--- packages/fr-config-promote/src/helpers/cli-options.js
+++ packages/fr-config-promote/src/helpers/cli-options.js
@@ -1,29 +1,33 @@
 const OPTION = {
-
   ID: "id",
   PROMOTE_DEPENDENCIES: "promote-dependencies",
   LIST: "list",
   REPORTID: "reportID",
+  IGNORE_ENCRYPTED_SECRETS: "ignore-encrypted-secrets",
 };
 
 const CLI_OPTIONS = {
   id: {
     alias: "i",
     describe: "ID",
-    type: "string", 
+    type: "string",
     demandOption: true,
   },
   list: {
     alias: "l",
     describe: "list",
     type: "boolean",
-
   },
   reportID: {
     alias: "r",
     describe: "Report ID",
-    type: "string", 
-    },
+    type: "string",
+  },
+  "ignore-encrypted-secrets": {
+    alias: "e",
+    describe: "Ignore encrypted secrets",
+    type: "boolean",
+  },
 };
 
 const cliOptions = (requestedOptions) => {

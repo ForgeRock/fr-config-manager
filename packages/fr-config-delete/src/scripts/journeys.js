5d81ac1e19d773277670261326bcc9ca0e941955
Tasos Kampas <36712117+TasosKampas@users.noreply.github.com>
2025-11-20 12:45:07 +0200

    fr-config-delete clean up (#292) (#312)
    * Remove unused scripts in fr-config-delete
    
    * Fix single journey deletion with multiple realms
    
    * Update package versions
    
    * Remove unused test scripted in delete package
    
    * Update package versions
    
    ---------
    
    Signed-off-by: Kevin Schneider <53996969+kevswiss@users.noreply.github.com>
    Co-authored-by: Kevin Schneider <53996969+kevswiss@users.noreply.github.com>

--- packages/fr-config-delete/src/scripts/journeys.js
+++ packages/fr-config-delete/src/scripts/journeys.js
@@ -1,238 +1,277 @@
 const {
-  restGet,
-  restDelete,
-  restPost,
+    restGet,
+    restDelete,
+    restPost,
 } = require("../../../fr-config-common/src/restClient");
 
-let tenantUrl, deleteInnerJourneys, dryRun, token, debug;
+const API_VERSION = "protocol=2.0,resource=1.0";
+const INNER_NODE = "InnerTreeEvaluatorNode"
+const AUTH_TREE_PATH = "realm-config/authentication/authenticationtrees"
 
 /**
  * Constructs the AM endpoint URL for a given realm and path.
  */
-function constructAmEndpoint(realm, path) {
-  return `${tenantUrl}/am/json/realms/root/realms/${realm}/${path}`;
+function constructAmEndpoint(tenantUrl, realm, path) {
+    return `${tenantUrl}/am/json/realms/root/realms/${realm}/${path}`;
 }
 
-/**
- * Logs a message if debug mode is enabled.
- */
-function debugLog(message) {
-  if (debug) {
-    console.log(message);
-  }
+async function getNodeTypes(tenantUrl, realm, token) {
+
+    const nodesEndpoint = constructAmEndpoint(
+        tenantUrl,
+        realm,
+        `${AUTH_TREE_PATH}/nodes`
+    );
+
+    const response = await restPost(
+        nodesEndpoint, {
+            _action: "getAllTypes",
+        },
+        null,
+        token,
+        API_VERSION
+    );
+
+    return response.data.result;
 }
 
-async function getNodeTypes(realm) {
-  const amEndpoint = `${tenantUrl}/am/json/realms/root/realms/${realm}/realm-config/authentication/authenticationtrees/nodes/?_action=getAllTypes`;
-  const response = await restPost(
-    amEndpoint,
-    null,
-    null,
-    token,
-    "protocol=2.1,resource=1.0"
-  );
+async function getNode(tenantUrl, realm, token, nodeId, nodeType) {
 
-  return response.data.result;
-}
+    const nodeIdEndpoint = constructAmEndpoint(
+        tenantUrl,
+        realm,
+        `${AUTH_TREE_PATH}/nodes/${nodeType}/${nodeId}`
+    );
 
-async function getNode(nodeId, nodeType, realm) {
-  const amEndpoint = `${tenantUrl}/am/json/realms/root/realms/${realm}/realm-config/authentication/authenticationtrees/nodes/${nodeType}/${nodeId}`;
-  const response = await restGet(
-    amEndpoint,
-    null,
-    token,
-    "protocol=2.1,resource=1.0"
-  );
-  return response.data;
-}
+    try {
+        const response = await restGet(nodeIdEndpoint, null, token, API_VERSION);
+        return response.data;
+    } catch (err) {
+        if (err.response && err.response.status === 404) {
+            console.error(`Error: Node with ID '${nodeId}' and type '${nodeType}' not found.`);
+            return null;
+        }
 
-/**
- * Deletes a node by its ID and type.
- */
-async function deleteNode(nodeId, nodeType, realm) {
-  const nodeUrl = constructAmEndpoint(
-    realm,
-    `realm-config/authentication/authenticationtrees/nodes/${nodeType}/${nodeId}`
-  );
-  try {
-    if (dryRun) {
-      console.log(`Dry run: Deleting node ${nodeId} of type ${nodeType}`);
-    } else {
-      await restDelete(nodeUrl, token, "protocol=2.1,resource=1.0", true);
+        console.error(`An unexpected error occurred while fetching node '${nodeId}': ${err.message}`);
+        throw err;
     }
-    debugLog(`Deleted node ${nodeId} of type ${nodeType}`);
-  } catch (err) {
-    console.error(`Error deleting node ${nodeId}: ${err}`);
-  }
-}
-
-async function processSingleJourney(
-  realm,
-  name,
-  endpoint,
-  accessToken,
-  innerJourneysFlag,
-  dryRunFlag,
-  debugFlag
-) {
-  if (!!endpoint) {
-    tenantUrl = endpoint;
-  }
-  if (!!accessToken) {
-    token = accessToken;
-  }
-  if (!!innerJourneysFlag) {
-    deleteInnerJourneys = innerJourneysFlag;
-  }
-  if (!!dryRunFlag) {
-    dryRun = dryRunFlag;
-  }
-  if (!!debugFlag) {
-    debug = debugFlag;
-  }
-  debugLog(`tenantUrl: ${tenantUrl}`);
-  debugLog(`Journey name: ${name}`);
-  const journeysUrl = constructAmEndpoint(
-    realm,
-    `realm-config/authentication/authenticationtrees/trees/${encodeURIComponent(
-      name
-    )}`
-  );
-  const response = await restGet(
-    journeysUrl,
-    null,
-    token,
-    "protocol=2.1,resource=1.0",
-    true
-  );
-  if (!response || !response.data) {
-    console.error(`No journey found with name ${name}`);
-    return;
-  }
-  const journey = response.data;
-
-  await processJourney(journey, realm, name);
 }
 
 /**
  * Processes a single journey and its nodes.
  */
-async function processJourney(journey, realm, name) {
-  for (const [nodeId, nodeInfo] of Object.entries(journey.nodes)) {
-    if (
-      !!name &&
-      !!deleteInnerJourneys &&
-      nodeInfo.nodeType === "InnerTreeEvaluatorNode"
-    ) {
-      const node = await getNode(nodeId, nodeInfo.nodeType, realm);
-      if (node.tree) {
-        await processSingleJourney(realm, node.tree);
-      } else {
-        console.log(
-          `Inner tree not found in node ${nodeId} of type ${nodeInfo.nodeType}`
-        );
-      }
-    }
-  }
+async function retrieveInnerTreeData(tenantUrl, token, realm, journeyName, deleteInnerJourneysFlag, dryRun) {
 
-  // Delete the journey
-  if (dryRun) {
-    console.log(`Dry run: Deleting journey ${journey._id}`);
-  } else {
-    //journey._id is the name of the journey, can contain trailing whitespace which must be preserved with encodeURIComponent
-    const journeyUrl = constructAmEndpoint(
-      realm,
-      `realm-config/authentication/authenticationtrees/trees/${encodeURIComponent(
-        journey._id
-      )}`
+    const journeyEndpoint = constructAmEndpoint(
+        tenantUrl,
+        realm,
+        `${AUTH_TREE_PATH}/trees/${encodeURIComponent(journeyName)}`
     );
-    await restDelete(journeyUrl, token, "protocol=2.1,resource=1.0");
-  }
 
-  // Cleanup nodes
-  for (const [nodeId, nodeInfo] of Object.entries(journey.nodes)) {
-    await deleteNode(nodeId, nodeInfo.nodeType, realm);
-  }
+    try {
+        const response = await restGet(journeyEndpoint, null, token, API_VERSION, true);
+        const journey = response.data;
+
+        await processJourney(journey, tenantUrl, realm, token, deleteInnerJourneysFlag, dryRun);
+
+    } catch (err) {
+        if (err.response && err.response.status === 404) {
+            console.error(`No imner journey found with name '${journeyName}'.`);
+            return;
+        }
+        console.error(`An unexpected error occurred: ${err.message}`);
+        throw err;
+    }
 }
 
 /**
- * Processes all journeys in a realm.
+ * Processes a single journey and its nodes.
  */
-async function processJourneys(journeys, realm, name) {
-  for (const journey of journeys) {
-    await processJourney(journey, realm, name);
-  }
+async function processJourney(journey, tenantUrl, realm, token, deleteInnerJourneysFlag, dryRun, counters) {
+    for (const [nodeId, nodeInfo] of Object.entries(journey.nodes)) {
+        if (!!deleteInnerJourneysFlag && nodeInfo.nodeType === INNER_NODE) {
+            const node = await getNode(tenantUrl, realm, token, nodeId, nodeInfo.nodeType);
+            if (node.tree) {
+                await retrieveInnerTreeData(tenantUrl, token, realm, node.tree, deleteInnerJourneysFlag, dryRun);
+            } else {
+                console.log(
+                    `Inner tree not found in node ${nodeId} of type ${nodeInfo.nodeType}`
+                );
+            }
+        }
+    }
+
+    const journeyUrl = constructAmEndpoint(
+        tenantUrl,
+        realm,
+        `${AUTH_TREE_PATH}/trees/${encodeURIComponent(journey._id)}`
+    );
+
+    if (dryRun) {
+        console.log(`Dry run: Deleting journey ${realm}/${journey._id}`);
+        counters.deletedJourneys++;
+    } else {
+        try {
+            await restDelete(journeyUrl, token, API_VERSION);
+            counters.deletedJourneys++;
+            console.log(`Deleting journey ${realm}/${journey._id}`);
+        } catch (err) {
+            console.error(`Failed to delete journey ${realm}/${journey._id}: `, err);
+        }
+    }
+
+
+    for (const [nodeId, nodeInfo] of Object.entries(journey.nodes)) {
+        if (dryRun) {
+            console.log(`Dry run: Deleting journey ${journey._id} node ${nodeId} of type ${nodeInfo.nodeType}`);
+            counters.deletedNodes++;
+        } else {
+            try {
+                await deleteNode(tenantUrl, realm, token, nodeId, nodeInfo.nodeType);
+                console.log(`Deleting journey ${journey._id} node: ${nodeId} of type ${nodeInfo.nodeType}`);
+                counters.deletedNodes++;
+            } catch (err) {
+                console.error(`Failed to delete journey ${journey._id} node ${nodeId} of type ${nodeInfo.nodeType}:`, err);
+            }
+        }
+    }
+
+    console.log(`Deleted all nodes from journey ${realm}/${journey._id}`);
+}
+/**
+ * Processes Journeys
+ */
+async function processJourneys(journeys, name, tenantUrl, realm, token, deleteInnerJourneysFlag, dryRun, counters) {
+
+    let matchFound = false;
+
+    for (const journey of journeys) {
+        const journeyName = journey._id;
+
+        if (name && name !== journeyName) {
+            continue;
+        }
+
+        matchFound = true;
+
+        try {
+            await processJourney(journey, tenantUrl, realm, token, deleteInnerJourneysFlag, dryRun, counters);
+        } catch (err) {
+            console.error(`Failed to delete journey ${journeyName} in /'${realm}':`, err);
+        }
+    }
+
+    if (name && !matchFound) {
+        console.log(`Warning: Journey with name '${name}' in /'${realm}' not found.`);
+    }
 }
 
 /**
  * Cleans up orphaned nodes in a realm.
  */
-async function cleanupNodes(realm) {
-  const response = await getNodeTypes(realm);
-  for (const nodeType of response) {
-    const nodeTypeUrl = constructAmEndpoint(
-      realm,
-      `realm-config/authentication/authenticationtrees/nodes/${nodeType._id}?_queryFilter=true`
-    );
-    const nodes = await restGet(nodeTypeUrl, null, token);
-    for (const node of nodes.data.result) {
-      await deleteNode(node._id, nodeType._id, realm);
+async function cleanupNodes(tenantUrl, realm, token, dryRun, counters) {
+    const response = await getNodeTypes(tenantUrl, realm, token);
+    for (const nodeType of response) {
+        const amEndpoint = constructAmEndpoint(tenantUrl,
+            realm,
+            `${AUTH_TREE_PATH}/nodes/${nodeType._id}`
+        );
+
+        const response = await restGet(
+            amEndpoint, {
+                _queryFilter: "true",
+            },
+            token
+        );
+
+        const nodes = response.data.result;
+
+        for (const node of nodes) {
+            if (dryRun) {
+                console.log(`Dry run: Deleting orphan node ${node._id} of type ${nodeType._id}`);
+                counters.deletedOrphanNodes++;
+                continue;
+            }
+            await deleteNode(tenantUrl, realm, token, node._id, nodeType._id);
+            counters.deletedOrphanNodes++;
+        }
     }
-  }
+    console.log(`Deleted all orphan nodes`);
+}
+
+/**
+ * Deletes a node by its ID and type.
+ */
+async function deleteNode(tenantUrl, realm, token, nodeId, nodeType) {
+    const nodeUrl = constructAmEndpoint(tenantUrl,
+        realm,
+        `${AUTH_TREE_PATH}/nodes/${nodeType}/${nodeId}`
+    );
+    try {
+        await restDelete(nodeUrl, token, API_VERSION, true);
+        console.log(`Deleting node ${nodeId} of type ${nodeType}`);
+    } catch (err) {
+        console.error(`Error deleting node ${nodeId}: ${err}`);
+    }
 }
 
 /**
  * Main function to delete journeys and optionally clean up nodes.
  */
-async function deleteJourneys(
-  url,
-  realms,
-  name,
-  deleteInnerJourneysFlag,
-  dryRunFlag,
-  debugFlag,
-  accessToken
-) {
-  tenantUrl = url;
-  deleteInnerJourneys = deleteInnerJourneysFlag;
-  dryRun = dryRunFlag;
-  debug = debugFlag;
-  token = accessToken;
+async function deleteJourneys(tenantUrl, realms, name, token, deleteInnerJourneysFlag, dryRun) {
 
-  debugLog(`tenantUrl: ${tenantUrl}`);
-  debugLog(`realms: ${realms}`);
-  debugLog(`name: ${name}`);
-  debugLog(`deleteInnerJourneys: ${deleteInnerJourneys}`);
-  debugLog(`token: ${token}`);
+    const counters = {
+        deletedJourneys: 0,
+        deletedNodes: 0,
+        deletedOrphanNodes: 0
+    };
 
-  if (!!name) {
-    if (realms.length > 1) {
-      console.error(
-        "Error: Cannot delete journey by name when multiple realms are provided"
-      );
-      process.exit(1);
-    }
-
-    await processSingleJourney(realms[0], name);
-  } else {
-    for (const realm of realms) {
-      try {
-        const journeysUrl = constructAmEndpoint(
-          realm,
-          "realm-config/authentication/authenticationtrees/trees?_queryFilter=true"
+    if (!!name) {
+        if (realms.length > 1) {
+        console.error(
+            "Error: Cannot delete journey by name when multiple realms are provided"
         );
-        const response = await restGet(journeysUrl, null, token);
-        const journeys = response.data.result;
-
-        await processJourneys(journeys, realm, name);
-        // Cleanup orphaned nodes
-        await cleanupNodes(realm);
-      } catch (err) {
-        console.error(`Error processing realm ${realm}: ${err}`);
-      }
+        process.exit(1);
+        }
     }
-  }
+
+    try {
+        for (const realm of realms) {
+
+            const amEndpoint = constructAmEndpoint(tenantUrl,
+                realm,
+                `${AUTH_TREE_PATH}/trees`
+            );
+
+            const response = await restGet(
+                amEndpoint, {
+                    _queryFilter: "true",
+                },
+                token
+            );
+
+            const journeys = response.data.result;
+
+            await processJourneys(journeys, name, tenantUrl, realm, token, deleteInnerJourneysFlag, dryRun, counters);
+
+            // Cleanup orphaned nodes
+            if (!name) {
+                console.log(`Cleanup orphaned nodes`);
+                await cleanupNodes(tenantUrl, realm, token, dryRun, counters);
+            }
+
+        }
+
+        if (!name && dryRun) {
+            console.log("--- Journey and Nodes Deletion Summary ---");
+            console.log(`Journeys deleted: ${counters.deletedJourneys}`);
+            console.log(`Nodes deleted: ${counters.deletedNodes}`);
+            console.log(`Orphan Nodes deleted: ${counters.deletedOrphanNodes}`);
+        }
+
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 module.exports.deleteJourneys = deleteJourneys;
-module.exports.processSingleJourney = processSingleJourney;
